afdfecbaf37fac93c5f249727281acc769108730
carrollh <carrollh@email.sc.edu>
2017-12-14 01:01:42 +0900

    fixed path issue, added messages during install

--- scripts/Install-DataKeeperSignal.ps1
+++ scripts/Install-DataKeeperSignal.ps1
@@ -3,20 +3,24 @@ Param(
     [String] $Path = "C:\Program Files (x86)\SIOS\DataKeeper_Signal"
 )
 
+# Use the ExtMirrBase location (if it exists) as a base location for installing dk signal
 if($env:ExtMirrBase -eq $Null) {
+	# DK not installed, so test default location to see if files were extracted successfully
     if(-Not (Test-Path -Path $Path)) {
-        Write-Error "DataKeeper_Signal files not found" 
+        Write-Error "DataKeeper_Signal files were not found."
+		Start-Sleep
         exit 1
     }
 } else {
+	# DK is installed, install dk signal to it's parent folder
     $path = "$env:ExtMirrBase\..\DataKeeper_Signal"
 }
 
 # get properites needed for Signal_iQ config from user
-$hostname = Read-Host "Please enter the hostname for your iQ environment"
-$password = Read-Host "Please enter the password for your iQ environment"
-$username = Read-Host "Please enter the admin username for your iQ environment"
-$environmentID =  Read-Host "Please enter your iQ environment ID (format: 123456789)"
+$environmentID =  Read-Host "Please enter the ENVIRONMENT ID for your iQ appliance (format: 123456789)"
+$hostname = Read-Host "Please enter the HOSTNAME for your iQ appliance"
+$username = Read-Host "Please enter the ADMIN USERNAME for your iQ appliance"
+$password = Read-Host "Please enter the ADMIN PASSWORD for your iQ appliance"
 
 # create new Signal_iQ config.ini file from sample
 $config = Get-Content -Path "$path\dist\library.zip\SignaliQ\config.sample.ini"
@@ -34,8 +38,10 @@ $password = $credential.GetNetworkCredential().Password
 # new scheduled task properties to run the ps script every 5 minutes after boot
 $path = "C:\Program Files (x86)\SIOS\DataKeeper_Signal"
 $action = New-ScheduledTaskAction -Execute "Powershell.exe" -Argument "'$path\Send-Signal.ps1' -PyModule '$path\dist\report_event.exe' -EnvironmentID $environmentID"
+
 $triggers = [System.Collections.ArrayList]@()
-# trigger repeats for 10 years because of differences between how PSv4 and later versions deal with TimeSpan.MaxValue
+
+# this trigger repeats for 10 years because of differences between how PSv4 and later versions deal with TimeSpan.MaxValue
 $triggers.Add((New-ScheduledTaskTrigger -Once -At ([System.DateTime]::Now) -RepetitionDuration (New-TimeSpan -Days 3650) -RepetitionInterval (New-TimeSpan -Minutes 5))) >$Null
 $triggers.Add((New-ScheduledTaskTrigger -AtStartup)) >$Null
 
